0d8f16056af9eb6f8850cb309f7dd10d23c2b0ba
moneromooo <moneromoo@nowhere.nowhere.nowhere>
2020-10-27 11:37:53 +0000

    protocol: detect and drop asshole peers
    defined as those who don't relay blocks for now.
    
    Using transactions would be much faster, but more error prone,
    as well as exploit prone.
    
    The score can be reused later to store in the peer list to
    affect selection probability.

--- src/cryptonote_core/cryptonote_core.cpp
+++ src/cryptonote_core/cryptonote_core.cpp
@@ -1545,7 +1545,7 @@ namespace cryptonote
       for(auto& tx:  txs)
         arg.b.txs.push_back({tx, crypto::null_hash});
 
-      m_pprotocol->relay_block(arg, exclude_context);
+      m_pprotocol->relay_block(arg, exclude_context, std::vector<boost::uuids::uuid>());
     }
     return true;
   }
