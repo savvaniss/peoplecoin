17949581a77587f687c727cb298bf5bdec51a98b
Jeffrey <jeffryan@tamu.edu>
2022-04-22 14:32:56 -0500

    EPEE: Remove gzip_encoding.h

--- contrib/epee/include/net/http_client.h
+++ contrib/epee/include/net/http_client.h
@@ -40,11 +40,6 @@
 
 #include "net_helper.h"
 #include "http_client_base.h"
-
-#ifdef HTTP_ENABLE_GZIP
-#include "gzip_encoding.h"
-#endif 
-
 #include "string_tools.h"
 #include "reg_exp_definer.h"
 #include "abstract_http_client.h"
@@ -758,13 +753,9 @@ namespace net_utils
 				boost::smatch result;						//   12      3
 				if(boost::regex_search( m_response_info.m_header_info.m_content_encoding, result, rexp_match_gzip, boost::match_default) && result[0].matched)
 				{
-#ifdef HTTP_ENABLE_GZIP
-					m_pcontent_encoding_handler.reset(new content_encoding_gzip(this, result[3].matched));
-#else
           m_pcontent_encoding_handler.reset(new do_nothing_sub_handler(this));
-          LOG_ERROR("GZIP encoding not supported in this build, please add zlib to your project and define HTTP_ENABLE_GZIP");
+          LOG_ERROR("GZIP encoding not supported");
           return false;
-#endif
 				}
 				else 
 				{
