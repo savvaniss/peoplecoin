b2e03a08eaa95707c16021917177ac64b46f50fc
XfedeX <fedemar568@gmail.com>
2023-01-14 13:43:46 +0100

    change code style to match wownero's

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -83,17 +83,17 @@
 
 #define DIFFICULTY_TARGET_V2                            300
 #define DIFFICULTY_TARGET_V1                            300
+#define DIFFICULTY_WINDOW_V4                            144 // 12 hours
 #define DIFFICULTY_WINDOW_V3                            144
 #define DIFFICULTY_WINDOW_V2                            60
 #define DIFFICULTY_WINDOW                               720 // blocks
-#define DIFFICULTY_WINDOW_V4                            144 // 12 hours
 #define DIFFICULTY_LAG                                  15  // !!!
 #define DIFFICULTY_CUT                                  60  // timestamps to cut after sorting
 #define DIFFICULTY_CUT_V2                               10  // used by v6 difficulty with v4 blocks count
+#define DIFFICULTY_BLOCKS_COUNT_V4                      DIFFICULTY_WINDOW_V4 + 1 // new difficulty doesn't have lag
 #define DIFFICULTY_BLOCKS_COUNT_V3                      DIFFICULTY_WINDOW_V3 + 1 // added +1 to make N=N
 #define DIFFICULTY_BLOCKS_COUNT_V2                      DIFFICULTY_WINDOW_V2 + 1 // added +1 to make N=N
 #define DIFFICULTY_BLOCKS_COUNT                         DIFFICULTY_WINDOW + DIFFICULTY_LAG
-#define DIFFICULTY_BLOCKS_COUNT_V4                      DIFFICULTY_WINDOW_V4 + 1 // new difficulty doesn't have lag
 #define DIFFICULTY_RESET_HEIGHT                         331170// ~July 4, 2021 Pool Independence Day
 #define DIFFICULTY_RESET_LEVEL                          100000000 // 100 mill
 
