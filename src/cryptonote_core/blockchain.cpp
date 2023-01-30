fb398910319b4bfc5b0ab769c258457ce166362d
wowario <wowario@protonmail.com>
2023-01-30 20:08:11 +0300

    add forks and checkpoints

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -5540,7 +5540,7 @@ void Blockchain::cancel()
 }
 
 #if defined(PER_BLOCK_CHECKPOINT)
-static const char expected_block_hashes_hash[] = "374d836b26c46b9d8fdbc977da7a89a43c2eefb65659901ee4d0053d302e1468";
+static const char expected_block_hashes_hash[] = "d01498f05e547e24a592412198b6321f5d3b01f74adcf34100781302af9b4a75";
 void Blockchain::load_compiled_in_block_hashes(const GetCheckpointsCallback& get_checkpoints)
 {
   if (get_checkpoints == nullptr || !m_fast_sync)
